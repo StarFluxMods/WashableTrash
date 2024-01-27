85dce1a270ad629d37a72c485f1e7eea1b23c7d5
Lachlan Leone <starfluxgames@gmail.com>
2024-01-28 09:37:33 +1100

    Updated .gitignore

--- Customs/WashedTrash.cs
+++ Customs/WashedTrash.cs
@@ -9,24 +9,10 @@ namespace KitchenWashableTrash.Customs
     public class WashedTrash : CustomItem
     {
         public override string UniqueNameID => "WashedTrash";
-        public override GameObject Prefab => MaterialUtils.AssignMaterialsByNames(Mod.Bundle.LoadAsset<GameObject>("Bin Bag"));
+        public override GameObject Prefab => Mod.Bundle.LoadAsset<GameObject>("Bin Bag").AssignVFXByNames().AssignMaterialsByNames();
 
         public override void OnRegister(Item gameDataObject)
         {
-            foreach (Transform child in gameDataObject.Prefab.GetComponentsInChildren<Transform>())
-            {
-                if (child.gameObject.name.ToLower().StartsWith("vfx_"))
-                {
-                    VisualEffect effect = child.gameObject.AddComponent<VisualEffect>();
-                    foreach (VisualEffectAsset asset in Resources.FindObjectsOfTypeAll<VisualEffectAsset>())
-                    {
-                        if (asset.name == "Knife Glint")
-                        {
-                            effect.visualEffectAsset = asset;
-                        }
-                    }
-                }
-            }
         }
     }
 }
